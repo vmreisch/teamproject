579ea884b26c8b0d3973fd43586680176c731339
vmreisch <vmreisch@gmail.com>
2023-11-01 13:27:50 -0600

    serializer added

--- team/views.py
+++ team/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from .models import Athlete
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import AthleteSerializer
 
-# Create your views here.
+class AthleteViewSet(viewsets.ModelViewSet):
+    queryset = Athlete.objects.all()
+    serializer_class = AthleteSerializer
+    permission_classes = [permissions.AllowAny]
